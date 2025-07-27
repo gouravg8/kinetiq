7ec78433a66e560b198475a82f0061c98d21b3aa
Gourav soni <gouravsoni8476@gmail.com>
2025-07-27 18:13:35 +0530

    fixed ui of week page

--- src/components/ui/StatsCards.tsx
+++ src/components/ui/StatsCards.tsx
@@ -3,53 +3,42 @@ import React from "react";
 import { Zap, Target, Dumbbell, Calendar } from "lucide-react";
 
 interface StatCardProps {
-  icon: React.ReactNode;
-  label: string;
-  value: string | number;
-  className?: string;
+	icon: React.ReactNode;
+	label: string;
+	value: string | number;
+	className?: string;
 }
 
 const StatCard = ({ icon, label, value, className = "" }: StatCardProps) => {
-  return (
-    <div className={`stats-card ${className}`}>
-      <div className="stats-icon">{icon}</div>
-      <div className="stats-content">
-        <div className="stats-label">{label}</div>
-        <div className="stats-value">{value}</div>
-      </div>
-    </div>
-  );
+	return (
+		<div
+			className={`bg-zinc-800 w-11/12 mx-auto rounded-md flex items-center py-3 px-4 gap-4 ${className}`}
+		>
+			<div className="p-2 rounded bg-zinc-700 text-(--primary-yellow)">
+				{icon}
+			</div>
+			<div className="">
+				<div className="text-sm font-light">{label}</div>
+				<div className="font-semibold">{value}</div>
+			</div>
+		</div>
+	);
 };
 
 const StatsCards = () => {
-  return (
-    <div className="stats-grid">
-      <StatCard
-        icon={<Zap size={24} />}
-        label="This Week"
-        value="0/7"
-        className="week-stats"
-      />
-      <StatCard
-        icon={<Target size={24} />}
-        label="Streak"
-        value="0 days"
-        className="streak-stats"
-      />
-      <StatCard
-        icon={<Dumbbell size={24} />}
-        label="Total Workouts"
-        value="6"
-        className="total-stats"
-      />
-      <StatCard
-        icon={<Calendar size={24} />}
-        label="This Month"
-        value="3"
-        className="month-stats"
-      />
-    </div>
-  );
+	const stats = [
+		{ icon: <Zap size={24} />, label: "This Week", value: "0 / 7" },
+		{ icon: <Target size={24} />, label: "Streak", value: "0 days" },
+		{ icon: <Dumbbell size={24} />, label: "Total Workouts", value: "6" },
+		{ icon: <Calendar size={24} />, label: "This Month", value: "3" },
+	];
+	return (
+		<div className="flex flex-col gap-3 mb-4">
+			{stats?.map((item) => (
+				<StatCard icon={item?.icon} label={item?.label} value={item?.value} />
+			))}
+		</div>
+	);
 };
 
 export default StatsCards;
