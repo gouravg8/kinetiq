bcf28158f32e578f8d9f3c01f4aa382f32ec4044
Gourav soni <gouravsoni8476@gmail.com>
2025-08-16 17:11:36 +0530

    added schema in adapter for auth

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,19 +1,17 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
-import * as schema from "@/db/schema";
+import { users, accounts, verifications, sessions } from "../../auth-schema";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL!,
-});
-
-const drizzleClient = drizzle(pool, { schema });
+const pool = new Pool({ connectionString: process.env.DATABASE_URL! });
+export const drizzleClient = drizzle(pool);
 
 export const adapter = drizzleAdapter(drizzleClient, {
-    schema: { users: schema.users, dailyLogs: schema.dailyLogs },
-    provider: "pg"
-    // db:drizzleClient, {
-    // schema,
-    // provider: "pg",
-
+    schema: {
+        users,
+        accounts,
+        verifications,
+        sessions
+    },
+    provider: "pg",
 });
