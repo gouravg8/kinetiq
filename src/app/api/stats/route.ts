35275abccc96336fbad13733f4327ba09475de52
Gourav soni <gouravsoni8476@gmail.com>
2025-11-02 12:37:27 +0530

    made the stats card workable

---
+++ src/app/api/stats/route.ts
@@ -0,0 +1,93 @@
+import { drizzleClient } from "@/db";
+import { dailyLogs } from "@/db/schema";
+import { auth } from "@/lib/auth";
+import dayjs from "dayjs";
+import { and, asc, eq, gte, lte, sql, sum } from "drizzle-orm";
+import { headers } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest, res: NextResponse) {
+	const session = await auth.api.getSession({
+		headers: await headers(),
+	});
+
+	if (!session) {
+		return new Response("Unauthorized", { status: 401 });
+	}
+
+	const url = new URL(req.url);
+	const fromDate = url.searchParams.get("fromDate");
+	const toDate = url.searchParams.get("toDate");
+
+	if (!fromDate || !toDate) {
+		return NextResponse.json(
+			{ error: "Missing query params. Use ?fromDate=...&toDate=..." },
+			{ status: 400 },
+		);
+	}
+
+	const parseFrom = new Date(fromDate);
+	const parseTo = new Date(toDate);
+	parseTo.setDate(parseTo.getDate() + 1);
+	parseTo.setMilliseconds(parseTo.getMilliseconds() - 1);
+
+	const parsedMonth = new Date(
+		dayjs(fromDate).startOf("month").format("YYYY-MM-DD"),
+	);
+	console.log({ parsedMonth });
+
+	const dateFilter = and(
+		eq(dailyLogs.userId, session.user.id),
+		gte(dailyLogs.date, parseFrom),
+		lte(dailyLogs.date, parseTo),
+	);
+
+	const data = await drizzleClient
+		.select()
+		.from(dailyLogs)
+		.where(dateFilter)
+		.orderBy(asc(dailyLogs.date));
+
+	const thisWeekResult = await drizzleClient
+		.select({
+			total: sql<number>`SUM(CASE WHEN ${dailyLogs.isCompleted} THEN 1 ELSE 0 END)`,
+		})
+		.from(dailyLogs)
+		.where(dateFilter);
+
+	const thisWeek = thisWeekResult[0].total || 0;
+
+	const streak = await drizzleClient
+		.select()
+		.from(dailyLogs)
+		// in this date range
+		.where(dateFilter)
+		.orderBy(asc(dailyLogs.date));
+
+	const totalWorkoutsResult = await drizzleClient
+		.select({
+			total: sql<number>`SUM(CASE WHEN ${dailyLogs.isCompleted} THEN 1 ELSE 0 END)`,
+		})
+		.from(dailyLogs);
+
+	const totalWorkouts = totalWorkoutsResult[0].total || 0;
+
+	const thisMonthResult = await drizzleClient
+		.select({
+			total: sql<number>`SUM(CASE WHEN ${dailyLogs.isCompleted} THEN 1 ELSE 0 END)`,
+		})
+		.from(dailyLogs)
+		.where(
+			and(
+				eq(dailyLogs.userId, session.user.id),
+				gte(dailyLogs.date, parsedMonth),
+			),
+		);
+
+	const thisMonth = thisMonthResult[0].total || 0;
+
+	return NextResponse.json(
+		{ thisWeek, totalWorkouts, thisMonth },
+		{ status: 200 },
+	);
+}
--- src/components/ui/StatsCards.tsx
+++ src/components/ui/StatsCards.tsx
@@ -1,6 +1,12 @@
 "use client";
+import { useDebounce } from "@/hooks/useDebounce";
+import { dateAtom } from "@/Jotai/timeAtom";
+import api from "@/lib/axios";
+import { useQuery } from "@tanstack/react-query";
+import dayjs from "dayjs";
+import { useAtomValue } from "jotai";
+import { Calendar, Dumbbell, Target, Zap } from "lucide-react";
 import React from "react";
-import { Zap, Target, Dumbbell, Calendar } from "lucide-react";
 
 interface StatCardProps {
 	icon: React.ReactNode;
@@ -27,11 +33,27 @@ const StatCard = ({ icon, label, value, className = "" }: StatCardProps) => {
 };
 
 const StatsCards = () => {
+	const currentDate = useAtomValue(dateAtom);
+	const currentMonth = dayjs(currentDate).format("MMM");
+
+	const debouncedDate = useDebounce(currentDate);
+
+	const { data, refetch } = useQuery({
+		queryKey: ["stats-card", debouncedDate],
+		queryFn: () => {
+			const startOfWeek = currentDate.startOf("week").format("YYYY-MM-DD");
+			const endOfWeek = currentDate.endOf("week").format("YYYY-MM-DD");
+
+			return api.get(`/api/stats?fromDate=${startOfWeek}&toDate=${endOfWeek}`)
+		},
+		retry: false
+	})
+
 	const stats = [
-		{ icon: <Zap size={24} />, label: "This Week", value: "0 / 7" },
+		{ icon: <Zap size={24} />, label: "This Week", value: data?.data?.thisWeek + " / 7" },
 		{ icon: <Target size={24} />, label: "Streak", value: "0 days" },
-		{ icon: <Dumbbell size={24} />, label: "Total Workouts", value: "6" },
-		{ icon: <Calendar size={24} />, label: "This Month", value: "3" },
+		{ icon: <Dumbbell size={24} />, label: "Total Workouts", value: data?.data?.totalWorkouts },
+		{ icon: <Calendar size={24} />, label: `This Month (${currentMonth})`, value: data?.data?.thisMonth },
 	];
 	return (
 		<div className="flex flex-col md:flex-row gap-3 mb-4">
