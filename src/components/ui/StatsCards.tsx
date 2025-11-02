02ebf6ae2f409c3cd99eda775260691fe63bd863
Gourav soni <gouravsoni8476@gmail.com>
2025-11-02 12:48:05 +0530

    fix duplicate stat cards

--- src/components/ui/StatsCards.tsx
+++ src/components/ui/StatsCards.tsx
@@ -13,12 +13,11 @@ interface StatCardProps {
 	label: string;
 	value: string | number;
 	className?: string;
-	key: string | number;
 }
 
 const StatCard = ({ icon, label, value, className = "" }: StatCardProps) => {
 	return (
-		<div key={value}
+		<div
 			className={`w-11/12 bg-zinc-800 mx-auto rounded-md flex items-center py-3 px-4 gap-4 ${className}`}
 		>
 			<div className="p-2 rounded bg-zinc-700 text-(--primary-yellow)">
@@ -38,27 +37,29 @@ const StatsCards = () => {
 
 	const debouncedDate = useDebounce(currentDate);
 
-	const { data, refetch } = useQuery({
+	const { data: statsData, isLoading } = useQuery({
 		queryKey: ["stats-card", debouncedDate],
-		queryFn: () => {
+		queryFn: async () => {
 			const startOfWeek = currentDate.startOf("week").format("YYYY-MM-DD");
 			const endOfWeek = currentDate.endOf("week").format("YYYY-MM-DD");
 
-			return api.get(`/api/stats?fromDate=${startOfWeek}&toDate=${endOfWeek}`)
+			const response = await api.get(`/api/stats?fromDate=${startOfWeek}&toDate=${endOfWeek}`);
+			return response.data;
 		},
 		retry: false
 	})
 
 	const stats = [
-		{ icon: <Zap size={24} />, label: "This Week", value: data?.data?.thisWeek + " / 7" },
+		{ icon: <Zap size={24} />, label: "This Week", value: `${statsData?.thisWeek ?? 0} / 7` },
 		{ icon: <Target size={24} />, label: "Streak", value: "0 days" },
-		{ icon: <Dumbbell size={24} />, label: "Total Workouts", value: data?.data?.totalWorkouts },
-		{ icon: <Calendar size={24} />, label: `This Month (${currentMonth})`, value: data?.data?.thisMonth },
+		{ icon: <Dumbbell size={24} />, label: "Total Workouts", value: statsData?.totalWorkouts ?? 0 },
+		{ icon: <Calendar size={24} />, label: `This Month (${currentMonth})`, value: statsData?.thisMonth ?? 0 },
 	];
+
 	return (
 		<div className="flex flex-col md:flex-row gap-3 mb-4">
 			{stats?.map((item) => (
-				<StatCard key={item.value} icon={item?.icon} label={item?.label} value={item?.value} />
+				<StatCard key={item.label} icon={item?.icon} label={item?.label} value={item?.value} />
 			))}
 		</div>
 	);
